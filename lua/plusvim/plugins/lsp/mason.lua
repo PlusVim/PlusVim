f8bde5b2c2ba152ad2d5c3f7ce6bf6e339b58bc5
Fiko767 <767fiko@gmail.com>
2025-05-17 09:06:57 +0200

    feat(lsp): add common LSP servers to ensure_installed

--- lua/plusvim/plugins/lsp/mason.lua
+++ lua/plusvim/plugins/lsp/mason.lua
@@ -1,5 +1,26 @@
 return {
-	"mason-org/mason.nvim",
-	event = "VeryLazy",
-	opts = {},
+	{
+		"mason-org/mason.nvim",
+		event = "VeryLazy",
+		opts = {},
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		event = "VeryLazy",
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+				"rust_analyzer",
+				"ts_ls",
+				"html",
+				"cssls",
+				"clangd",
+				"yamlls",
+				"gopls",
+				"jsonls",
+			},
+			automatic_installation = true,
+		},
+	},
 }
