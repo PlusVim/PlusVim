1df4e87721185e8aeeb096f1e048631413036e17
Fiko767 <767fiko@gmail.com>
2025-05-18 11:49:19 +0200

    feat: add lualine

---
+++ lua/plusvim/plugins/lualine.lua
@@ -0,0 +1,43 @@
+return {
+	"nvim-lualine/lualine.nvim",
+	event = "VeryLazy",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		extensions = {
+			"aerial",
+			"chadtree",
+			"ctrlspace",
+			"fern",
+			"fugitive",
+			"fzf",
+			"lazy",
+			"man",
+			"mason",
+			"mundo",
+			"neo-tree",
+			"nvim-tree",
+			"oil",
+			"overseer",
+			"quickfix",
+			"symbols-outline",
+			"toggleterm",
+			"trouble",
+		},
+		disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+		sections = {
+			lualine_c = {
+				{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+				"filename",
+			},
+			lualine_y = {
+				{ "progress", separator = " ", padding = { left = 1, right = 0 } },
+				{ "location", padding = { left = 0, right = 1 } },
+			},
+			lualine_z = {
+				function()
+					return " " .. os.date("%R")
+				end,
+			},
+		},
+	},
+}
