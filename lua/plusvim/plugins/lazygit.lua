eb90031df0ffa02b5b9ababadea95be62c43785b
Fiko767 <767fiko@gmail.com>
2025-05-21 12:34:05 +0200

    feat: replaced kdheepak/lazygit.nvim with snacks.nvim lazygit

--- lua/plusvim/plugins/lazygit.lua
+++ lua/plusvim/plugins/lazygit.lua
@@ -1,20 +1,28 @@
 return {
-	"kdheepak/lazygit.nvim",
+	---@param opts? snacks.lazygit.Config
+	"folke/snacks.nvim",
 	event = "VeryLazy",
-	cmd = {
-		"LazyGit",
-		"LazyGitConfig",
-		"LazyGitCurrentFile",
-		"LazyGitFilter",
-		"LazyGitFilterCurrentFile",
-	},
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-	},
 	keys = {
-		{ "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
-		{ "<leader>gG", "<cmd>LazyGitCurrentFile<cr>", desc = "Lazygit (current file)" },
-		{ "<leader>gf", "<cmd>LazyGitFilter<cr>", desc = "Lazygit (filter)" },
-		{ "<leader>gF", "<cmd>LazyGitFilterCurrentFile<cr>", desc = "Lazygit (filter file)" },
+		{
+			"<leader>gg",
+			function()
+				Snacks.lazygit.open()
+			end,
+		},
+		{
+			"<leader>gl",
+			function()
+				Snacks.lazygit.log()
+			end,
+		},
+		{
+			"<leader>gf",
+			function()
+				Snacks.lazygit.log_file()
+			end,
+		},
+	},
+	opts = {
+		lazygit = {},
 	},
 }
